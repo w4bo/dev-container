ad392a15582dff2eb13014c8282c68e85006e97c
Matteo Francia <m.francia@unibo.it>
2024-03-07 16:22:32 +0100

    fix(Dockerfile): fix the path of the .ssh folder

--- config.sh
+++ config.sh
@@ -9,7 +9,7 @@ if [[ $PUBLIC_KEY ]]; then
 else
     echo "PUBLIC_KEY is not set"
 fi
-chown -R dev:dev .ssh
+chown -R dev:dev /home/dev/.ssh
 chmod 600 $file
 # Run SSH server
 /usr/sbin/sshd -D -e
